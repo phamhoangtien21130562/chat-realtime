090689fbf62e318c4bcf80733fddd397fc9a2fa0
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-04-30 12:10:32 +0700

    confix mainChat

--- src/components/mainChat/MainChat.jsx
+++ src/components/mainChat/MainChat.jsx
@@ -2,7 +2,24 @@ import '../../assets/style/mainChat.css'
 
 const MainChat = () => {
   return(
-      <div className='mainChat'>MainChat</div>
+      <div className='mainChat'>
+        <div className="topChat">
+          <div className="user">
+            <img src="/img/avata.png" alt=""/>
+            <div className="texts">
+              <span>OzuSus</span>
+              <p>Lorem ipsum dolor sit amet</p>
+            </div>
+          </div>
+          <div className="icon">
+            <img src="/img/phone.png" alt=""/>
+            <img src="/img/video.png" alt=""/>
+            <img src="/img/info.png" alt=""/>
+          </div>
+        </div>
+        <div className="centerChat"></div>
+        <div className="bottomChat"></div>
+      </div>
   )
 }
 export default MainChat
