367a3b7acd8c42168014cd9b22acbbd6fb29149f
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-04-30 13:50:51 +0700

    handle emoji picker

--- src/components/mainChat/MainChat.jsx
+++ src/components/mainChat/MainChat.jsx
@@ -1,6 +1,13 @@
 import '../../assets/style/mainChat.css'
-
+import EmojiPicker from "emoji-picker-react";
+import {useState} from "react";
 const MainChat = () => {
+  const [openEmoji,setOpenEmoji] = useState(false)
+  const [emojiToText, setEmojiToText] = useState("")
+  const showEmoji = e => {
+    setEmojiToText(prev=>prev + e.emoji);
+    setOpenEmoji(false)
+  };
   return(
       <div className='mainChat'>
         <div className="topChat">
@@ -8,7 +15,7 @@ const MainChat = () => {
             <img src="/img/avata.png" alt=""/>
             <div className="texts">
               <span>OzuSus</span>
-              <p>Lorem ipsum dolor sit amet</p>
+              <p>Helllo World</p>
             </div>
           </div>
           <div className="icon">
@@ -18,7 +25,25 @@ const MainChat = () => {
           </div>
         </div>
         <div className="centerChat"></div>
-        <div className="bottomChat"></div>
+        <div className="bottomChat">
+            <div className="icons">
+                <img src="/img/img.png" alt=""/>
+                <img src="/img/camera.png" alt=""/>
+                <img src="/img/mic.png" alt=""/>
+            </div>
+            <input type="text" placeholder="Write your message here"
+                   value={emojiToText} onChange={e=> setEmojiToText(e.target.value)}
+            />
+            <div className="emoji">
+                <img src="/img/emoji.png" alt=""
+                     onClick={() =>setOpenEmoji((prev) => !prev)}
+                />
+                <div className="emojiPicker">
+                    <EmojiPicker open={openEmoji} onEmojiClick={showEmoji}/>
+                </div>
+            </div>
+            <button className="sendButton">Send</button>
+        </div>
       </div>
   )
 }
