02ab1440542097d29599bd78a8d5090a01ca6931
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-06-05 20:48:31 +0700

    handle register

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,32 +1,99 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../assets/style/register.css'
+
 const RegisterForm = () => {
-  return (
-    <html>
-      <head>
-        <title>Đăng kí</title>
-        <link rel="stylesheet" href="register.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,400i|Noto+Sans:400,400i,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet" />
-      </head>
-      <body>
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [socket, setSocket] = useState(null);
+    const [usernameTouched, setUsernameTouched] = useState(false);
+    const [passwordTouched, setPasswordTouched] = useState(false);
+    const [notification, setNotification] = useState('');
+    useEffect(() => {
+        const newSocket = new WebSocket("ws://140.238.54.136:8080/chat/chat");
+
+        newSocket.addEventListener("open", (event) => {
+            console.log("Kết nối WebSocket đã được thiết lập", event);
+            setSocket(newSocket);
+        });
+
+        return () => {
+            // Đóng kết nối WebSocket khi component bị hủy
+            newSocket.close();
+        };
+    }, []);
+
+    const handleRegister = () => {
+        // Gửi yêu cầu đăng nhập đến server WebSocket
+        const register = {
+            action: "onchat", data: {
+                event: "REGISTER", data: {
+                    user: username, pass: password,
+                },
+            },
+        };
+        socket.send(JSON.stringify(register));
+    };
+
+    // Sau khi đăng nhập thành công, set socket và lưu trữ thông tin đăng nhập
+    useEffect(() => {
+        if (socket) {
+            socket.onmessage = (event) => {
+                const responseData = JSON.parse(event.data);
+                if (responseData && responseData.status === "success") {
+                    // Đăng kí thành công
+                    setNotification('Đăng ký thành công!');
+                } else {
+                    setNotification('Tài khoản đã tồn tại!');
+                }
+            };
+        }
+    }, [socket]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleRegister();
+    };
+    const handleUserBlur = () => {
+        setUsernameTouched(true);
+    };
+
+    const handlePassBlur = () => {
+        setPasswordTouched(true);
+    };
+    return (
         <div className="to">
-          <div className="form">
-            <h2>Đăng ký tài khoản</h2>
-            <i className="fab fa-app-store-ios"></i>
-            <label style={{marginLeft: '-120px'}}> Username:</label>
-            <input type="text" name="hoten" />
-            <label style={{marginLeft: '-120px'}}> Password:</label>
-            <input type="text" name="pass" />	
-            <label style={{marginLeft: '-150px'}}> Email:</label>
-            <input type="text" name="email" /> 
-            <input id="submit" type="submit" name="submit" value="Gửi" />
-          </div>				
+            <div className="form">
+                <form className="register_form" onSubmit={handleSubmit}>
+                    <h2>Đăng ký tài khoản</h2>
+                    <i className="fab fa-app-store-ios"></i>
+                    <div className="username_Input">
+                        <label style={{marginLeft: '0px'}}> Username:</label>
+                        <input value={username}
+                               placeholder="Tên đăng nhập"
+                               type="text"
+                               onChange={(e) => setUsername(e.target.value)}
+                               onBlur={handleUserBlur}
+                               name="hoten"/>
+                    </div>
+                    <div className="pass_Input">
+                        <label style={{marginLeft: '0px'}}> Password:</label>
+                        <input value={password}
+                               type="password"
+                               placeholder="Mật khẩu"
+                               onChange={(e) => setPassword(e.target.value)}
+                               onBlur={handlePassBlur}
+                               name="pass"/>
+                    </div>
+                    <input id="submit" type="submit" name="submit" value="Đăng kí"/>
+                    <div className="login_ref">
+                        <p>Bạn đã có tài khoản?<a href="/" className="">Đăng nhập!</a></p>
+                    </div>
+                </form>
+
+
+            </div>
         </div>
-      </body>
-    </html>
-  );
+    );
 }
 
 export default RegisterForm;
