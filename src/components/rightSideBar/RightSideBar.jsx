63c685c267fa46d98889e4acae1bc5068e807e9e
buihuutrii <21130575@st.hcmuaf.edu.vn>
2024-06-07 12:22:13 +0700

    chuc nang logout

--- src/components/rightSideBar/RightSideBar.jsx
+++ src/components/rightSideBar/RightSideBar.jsx
@@ -1,7 +1,37 @@
-import '../../assets/style/rightSideBar.css'
+import React, { useState, useEffect } from 'react';
+import '../../assets/style/rightSideBar.css';
+import { useNavigate } from 'react-router-dom';
+
+const RightSideBar = ({ socket }) => {
+    const navigate = useNavigate();
+    const [logoutNotification, setLogoutNotification] = useState('');
+
+    const handleLogout = () => {
+        if (socket) {
+            const logoutData = {
+                action: "onchat",
+                data: {
+                    event: "LOGOUT"
+                }
+            };
+            socket.send(JSON.stringify(logoutData));
+        }
+
+        setLogoutNotification('Bạn đã đăng xuất thành công!');
+        {/* tro ve login */}
+        navigate('/');
+    };
+
+    useEffect(() => {
+        if (logoutNotification !== '') {
+            const timeout = setTimeout(() => {
+                setLogoutNotification('');
+            }, 3000); // 3 seconds
+            return () => clearTimeout(timeout);
+        }
+    }, [logoutNotification]);
 
 
-const RightSideBar = () => {
     return(
         <div className='rightSideBar'>
             <div className="user">
@@ -82,9 +112,13 @@ const RightSideBar = () => {
                     </div>
                 </div>
                 <button>Block User</button>
-                <button className="logOutBtn">Log out </button>
+                <button onClick={handleLogout} className="logOutBtn">Log out</button>
+                {logoutNotification && (
+                    <div className="logoutNotification">{logoutNotification}</div>
+                )}
             </div>
         </div>
-    )
-}
-export default RightSideBar
+    );
+};
+
+export default RightSideBar;
